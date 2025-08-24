734ebed9c5fa861c1c1ce143a288541360b2bd44
Arnold Tharrington <arnoldt@ornl.gov>
2025-08-24 13:03:11 -0400

    Transient commit.

--- Python/create_python_virtual_env.sh
+++ Python/create_python_virtual_env.sh
@@ -1,7 +1,20 @@
-# This scripts builds a pyhton virtual environment. 
+# This scripts builds a python virtual environment. 
 #
-declare -r venv_dir="${AT_PYTHON_VENV}"/lammps_mixed_precision
+#
 
+# Declare and create the virtual environment directory.
+declare -r venv_dir="${AT_PYTHON_VENV}"/lammps_mixed_precision
 mkdir -p ${venv_dir}
+
+#Change to the virtual environment directory and
+#run the command to create the Python environment.
 cd ${venv_dir}
 python3 -m venv ./
+
+# Activate the Python virtual environment.
+source ./activate
+
+# Below are commonly used python packages.
+declare -A python_packages = ( [numpy] = "yes" 
+                               [scipy] = "yes" 
+                               [mypy] = "yes" )
